4bdd0ebf65bf71c0422dffecf3fad05214b7c30d
drew-qhr <drew.hawken@qhrtech.com>
2025-02-18 13:32:13 -0800

    update repos to clone

--- clone.sh
+++ clone.sh
@@ -1,91 +1,67 @@
 #!/bin/sh
 
 echo "Cloning repositories..."
-
-CODE=$HOME/Code
-SITES=$HOME/Herd
-BLADE=$CODE/blade-ui-kit
-LARAVEL=$CODE/laravel
+mdir code
+CODE=$HOME/code
+cd $CODE
 
 # Sites
-git clone git@github.com:blade-ui-kit/blade-ui-kit.com.git $SITES/blade-ui-kit.com
-git clone git@github.com:laravel/blog.laravel.com.git $SITES/blog.laravel.com
-git clone git@github.com:blade-ui-kit/demo.git $SITES/demo.blade-ui-kit.com
-git clone git@github.com:driesvints/driesvints.com.git $SITES/driesvints.com
-git clone git@github.com:laravel/envoyer.git $SITES/envoyer
-git clone git@github.com:eventyio/eventy.io.git $SITES/eventy.io
-git clone git@github.com:laravel/forge.git $SITES/forge
-git clone git@github.com:fullstackeurope/fullstackeurope.com.git $SITES/fullstackeurope.com
-git clone git@github.com:laravel/laravel.com.git $SITES/laravel.com
-git clone git@github.com:laravelio/laravel.io.git $SITES/laravel.io
-git clone git@github.com:laravelio/paste.laravel.io.git $SITES/paste.laravel.io
-git clone git@github.com:laravel/nova.laravel.com.git $SITES/nova
-git clone git@github.com:laravel/spark.laravel.com.git $SITES/spark.laravel.com
-git clone git@github.com:laravel/vapor.git $SITES/vapor
-
-# Personal
-git clone git@github.com:lmsqueezy/laravel.git $CODE/lmsqueezy-laravel
-git clone git@github.com:driesvints/vat-calculator.git $CODE/vat-calculator
-
-# Blade UI Kit
-git clone git@github.com:blade-ui-kit/blade-docs.git $BLADE/blade-docs
-git clone git@github.com:blade-ui-kit/blade-heroicons.git $BLADE/blade-heroicons
-git clone git@github.com:blade-ui-kit/blade-icons.git $BLADE/blade-icons
-git clone git@github.com:blade-ui-kit/blade-ui-kit.git $BLADE/blade-ui-kit
-git clone git@github.com:blade-ui-kit/docs.git $BLADE/docs
-
-# Laravel
-git clone git@github.com:laravel/beep.git $LARAVEL/beep
-git clone git@github.com:laravel/breeze.git $LARAVEL/breeze
-git clone git@github.com:laravel/breeze-next.git $LARAVEL/breeze-next
-git clone git@github.com:laravel/browser-kit-testing.git $LARAVEL/browser-kit-testing
-git clone git@github.com:laravel/cashier-stripe.git $LARAVEL/cashier-stripe
-git clone git@github.com:laravel/cashier-paddle.git $LARAVEL/cashier-paddle
-git clone git@github.com:laravel/docs.git $LARAVEL/docs
-git clone git@github.com:laravel/dusk.git $LARAVEL/dusk
-git clone git@github.com:laravel/echo.git $LARAVEL/echo
-git clone git@github.com:laravel/envoy.git $LARAVEL/envoy
-git clone git@github.com:laravel/folio.git $LARAVEL/folio
-git clone git@github.com:laravel/forge-cli.git $LARAVEL/forge-cli
-git clone git@github.com:laravel/forge-sdk.git $LARAVEL/forge-sdk
-git clone git@github.com:laravel/fortify.git $LARAVEL/fortify
-git clone git@github.com:laravel/framework.git $LARAVEL/framework
-git clone git@github.com:laravel/helpers.git $LARAVEL/helpers
-git clone git@github.com:laravel/horizon.git $LARAVEL/horizon
-git clone git@github.com:laravel/installer.git $LARAVEL/installer
-git clone git@github.com:laravel/jetstream.git $LARAVEL/jetstream
-git clone git@github.com:laravel/jetstream-docs.git $LARAVEL/jetstream-docs
-git clone git@github.com:laravel/laravel.git $LARAVEL/laravel
-git clone git@github.com:laravel/legacy-factories.git $LARAVEL/legacy-factories
-git clone git@github.com:laravel/lumen.git $LARAVEL/lumen
-git clone git@github.com:laravel/lumen-framework.git $LARAVEL/lumen-framework
-git clone git@github.com:laravel/nova.git $LARAVEL/nova
-git clone git@github.com:laravel/octane.git $LARAVEL/octane
-git clone git@github.com:laravel/package-template.git $LARAVEL/package-template
-git clone git@github.com:laravel/pail.git $LARAVEL/pail
-git clone git@github.com:laravel/passport.git $LARAVEL/passport
-git clone git@github.com:laravel/pennant.git $LARAVEL/pennant
-git clone git@github.com:laravel/pint.git $LARAVEL/pint
-git clone git@github.com:laravel/precognition.git $LARAVEL/precognition
-git clone git@github.com:laravel/prompts.git $LARAVEL/prompts
-git clone git@github.com:laravel/pulse.git $LARAVEL/pulse
-git clone git@github.com:laravel/sail.git $LARAVEL/sail
-git clone git@github.com:laravel/sail-server.git $LARAVEL/sail-server
-git clone git@github.com:laravel/sanctum.git $LARAVEL/sanctum
-git clone git@github.com:laravel/scout.git $LARAVEL/scout
-git clone git@github.com:laravel/serializable-closure.git $LARAVEL/serializable-closure
-git clone git@github.com:laravel/slack-notification-channel.git $LARAVEL/slack-notification-channel
-git clone git@github.com:laravel/socialite.git $LARAVEL/socialite
-git clone git@github.com:laravel/spark-next-docs.git $LARAVEL/spark-next-docs
-git clone git@github.com:laravel/spark-paddle.git $LARAVEL/spark-paddle
-git clone git@github.com:laravel/spark-stripe.git $LARAVEL/spark-stripe
-git clone git@github.com:laravel/telescope.git $LARAVEL/telescope
-git clone git@github.com:laravel/tinker.git $LARAVEL/tinker
-git clone git@github.com:laravel/ui.git $LARAVEL/ui
-git clone git@github.com:laravel/valet.git $LARAVEL/valet
-git clone git@github.com:laravel/vapor-cli.git $LARAVEL/vapor-cli
-git clone git@github.com:laravel/vapor-core.git $LARAVEL/vapor-core
-git clone git@github.com:laravel/vapor-ui.git $LARAVEL/vapor-ui
-git clone git@github.com:laravel/vite-plugin.git $LARAVEL/vite-plugin
-git clone git@github.com:livewire/volt.git $LARAVEL/volt
-git clone git@github.com:laravel/vonage-notification-channel.git $LARAVEL/vonage-notification-channel
+git clone git@github.com:qhrtech/eqv-rails.git
+git clone git@github.com:qhrtech/medeo-iris.git
+git clone git@github.com:qhrtech/adb2c-examples.git
+git clone git@github.com:qhrtech/Openfire.git
+git clone git@github.com:qhrtech/idp.git
+git clone git@github.com:qhrtech/octopus-function.git
+git clone git@github.com:qhrtech/developer.medeohealth.com.git
+git clone git@github.com:qhrtech/xmpp-js.git
+git clone git@github.com:qhrtech/cat-main.git
+git clone git@github.com:qhrtech/medeo-deploy-tasks.git
+git clone git@github.com:qhrtech/arch-tech-radar.git
+git clone git@github.com:qhrtech/medeo-provider-ui.git
+git clone git@github.com:qhrtech/accuro-hosted.git
+git clone git@github.com:qhrtech/medeo-skin.git
+git clone git@github.com:qhrtech/solvent.git
+git clone git@github.com:qhrtech/idp-user-migrator.git
+git clone git@github.com:qhrtech/medeo-ebooking.git
+git clone git@github.com:qhrtech/stargate.git
+git clone git@github.com:qhrtech/stargate/accuro-communication-engine.git
+git clone git@github.com:qhrtech/amos.git
+git clone git@github.com:qhrtech/workstation-vm.git
+git clone git@github.com:qhrtech/medeo-admin.git
+git clone git@github.com:qhrtech/eprescribe-outbound.git
+git clone git@github.com:qhrtech/registry-webapp.git
+git clone git@github.com:qhrtech/medeo-workstation.git
+git clone git@github.com:qhrtech/acs-function.git
+git clone git@github.com:qhrtech/notification-microservices.git
+git clone git@github.com:qhrtech/medeo-rna.git
+git clone git@github.com:qhrtech/arch-async-api.git
+git clone git@github.com:qhrtech/medeo-e2e.git
+git clone git@github.com:qhrtech/medeo-function-apps.git
+git clone git@github.com:qhrtech/accuro.git
+git clone git@github.com:qhrtech/cat-medeo-admin-tools.git
+git clone git@github.com:qhrtech/medeo-rabbit-common.git
+git clone git@github.com:qhrtech/arch-messaging-examples.git
+git clone git@github.com:qhrtech/event-broker.git
+git clone git@github.com:qhrtech/angular-sso-demo.git
+git clone git@github.com:qhrtech/qhr-openapi.git
+git clone git@github.com:qhrtech/common-message-orchestrator.git
+git clone git@github.com:qhrtech/medeo-android.git
+git clone git@github.com:qhrtech/healthmail-mercury.git
+git clone git@github.com:qhrtech/conferencing-microservices.git
+git clone git@github.com:qhrtech/medeo-sclera.git
+git clone git@github.com:qhrtech/api-management-developer-portal.git
+git clone git@github.com:qhrtech/codescope.git
+git clone git@github.com:qhrtech/terraform-modules.git
+git clone git@github.com:qhrtech/create-user-function.git
+git clone git@github.com:qhrtech/arch-diagrams.git
+git clone git@github.com:qhrtech/medeo-rails/database.git
+git clone git@github.com:qhrtech/medeo-rails.git
+git clone git@github.com:qhrtech/prescribeit-inbound.git
+git clone git@github.com:qhrtech/medeo-faye-server.git
+git clone git@github.com:qhrtech/medeo-marrow.git
+git clone git@github.com:qhrtech/arch-docker.git
+git clone git@github.com:qhrtech/qhr-fhir-profiles.git
+git clone git@github.com:qhrtech/medeo-bones.git
+git clone git@github.com:qhrtech/prescribeit-okta-tokenhook.git
+git clone git@github.com:qhrtech/accuro-communication-engine.git
+git clone git@github.com:qhrtech/web-pub-sub-poc.git
